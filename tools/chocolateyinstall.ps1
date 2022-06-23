a093bb1b692b3776f3ee87299e9d9170db5bd8a4
Tomio <mail@tomio.fun>
2022-06-23 15:36:13 +0800

    fix

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
     PackageName    = $env:ChocolateyPackageName
     Destination    = $toolsDir
     FileFullPath   = Join-Path $toolsDir 'nixpacks-v0.1.0-i686-pc-windows-msvc.zip'
-    FileFullPath64 = Join-Path $toolsDir 'nixpacks-v0.1.0-aarch64-pc-windows-msvc.zip'
+    FileFullPath64 = Join-Path $toolsDir 'nixpacks-v0.1.0-x86_64-pc-windows-msvc.zip'
 }
 
 #Remove old versions of nixpacks in the tools directory
--- update.ps1
+++ update.ps1
@@ -24,7 +24,7 @@ function global:au_BeforeUpdate {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     $url_base = 'https://github.com'
-    $urls =  $download_page.Links | Where-Object href -match 'pc-windows-msvc' | ForEach-Object href | Select-Object -First 2
+    $urls =  $download_page.Links | Where-Object href -match 'x86_64-pc-windows-msvc' | ForEach-Object href | Select-Object -First 2
     $url32 = $url_base + $urls.Where({ $_ -match 686 }) 
     $url64 = $url_base + $urls.Where({ $_ -match 64 })
     
